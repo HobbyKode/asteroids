eaf4b5bbd73f69d74b93321b5d6330be03157c44
HobbyKode <speedemoniak@yahoo.fr>
2025-02-17 11:52:16 -0400

    At last, a screen! But it's black...

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ import pygame
 from constants import *
 
 def main():
+    #Initialize Pygame
+    pygame.init()
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((1,1,1))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
