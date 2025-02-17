2e95f550ab6812bd45f2d4900ed5462d9896526b
HobbyKode <speedemoniak@yahoo.fr>
2025-02-17 16:39:24 -0400

    My sprite is created! Now if I could move it...

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     #Initialize Pygame
     pygame.init()
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -16,13 +18,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((1,1,1))
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         frame_time = clock.tick(60)
         dt = frame_time / 1000
-
-
+        
 if __name__ == "__main__":
     main()
